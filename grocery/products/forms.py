b25491480a406075712996b204b1c2f6c1558664
Caren-Koroeny <carensiya5@gmail.com>
2022-06-16 11:34:32 +0300

    product forms

---
+++ grocery/products/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,IntegerField,TextAreaField,SubmitField, DateField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField
+
+class
